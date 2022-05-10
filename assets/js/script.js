56edda5bdb88ac98f5ef1e3199cfb2c3230e2801
ViktorMathe <matheviktor@icloud.com>
2022-05-10 22:01:05 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+
+function runGame{
+
+function runGame(:void
+    let num1= Math.floor(Math.random()*25)+1;
+    let num2= Math.floor(Math.random()*25)+1;
+}
+
+function checkAnswer(){
+
+}
+
+function calculateCorrectAnswer(){
+
+}
+
+function incrementScore(){
+
+}
+
+function incrementWrongAnswer(){
+
+}
+
+function displayAdditionQuestion(){
+
+}
+
+function displaySubtractQuestion(){
+
+} 
+
+function displayMultiplyQuestion(){
+
+}
